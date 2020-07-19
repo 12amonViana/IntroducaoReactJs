41f2635392d4a3048b3afd8569b5f689e268e4ce
12amonViana <marcos.ramon.viana@gmail.com>
2020-07-19 20:35:09 -0300

    Criando componentes

---
+++ src/components/basicos/Primeiro.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+// function Primeiro(){
+//     return(
+//         <>
+//             <h1>Primeiro</h1>
+//             <h2>Exemplo de um componente React</h2>
+//         </>
+//     )
+// }
+
+// export default Primeiro
+
+export default () =>
+    <>
+        <h1>Primeiro Componente (Arrow)</h1>
+        <h2>Exemplo de um componente React</h2>
+    </>
